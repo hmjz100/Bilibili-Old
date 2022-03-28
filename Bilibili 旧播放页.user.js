3e92e309abc1111a4a44dd19f0741c2b0fc1ac00
hmjz100 <98228280+hmjz100@users.noreply.github.com>
2022-03-28 12:55:22 +0800

    Update Bilibili 旧播放页.user.js

--- Bilibili 旧播放页.user.js
+++ Bilibili 旧播放页.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Bilibili 旧播放页（改）
 // @namespace    MotooriKashin
-// @version      7.2.2
-// @description  恢复Bilibili旧版页面，为了那些念旧的人。
+// @version      7.2.3
+// @description  恢复Bilibili旧版页面，为了那些念旧的人。（侵删）
 // @author       MotooriKashin，wly5556，hmjz100
-// @homepage     https://github.com/MotooriKashin/Bilibili-Old
-// @supportURL   https://github.com/MotooriKashin/Bilibili-Old/issues
+// @homepage     https://github.com/hmjz100/Bilibili-Old
+// @supportURL   https://github.com/hmjz100/Bilibili-Old/issues
 // @icon         https://static.hdslb.com/images/favicon.ico
 // @match        *://*.bilibili.com/*
 // @connect      *
@@ -6765,7 +6765,7 @@ option {
         type: "sort",
         key: "toast",
         label: "浮动通知",
-        sub: '使用toastr作为浮动通知</br>去<a href="//github.com/CodeSeven/toastr">Github</a>上查看toastr',
+        sub: '使用toastr作为浮动通知</br>在<a href="//github.com/CodeSeven/toastr">Github</a>上查看toastr',
         svg: '<svg viewBox="0 0 16 16"><path d="M8 16a2 2 0 001.985-1.75c.017-.137-.097-.25-.235-.25h-3.5c-.138 0-.252.113-.235.25A2 2 0 008 16z"></path><path fill-rule="evenodd" d="M8 1.5A3.5 3.5 0 004.5 5v2.947c0 .346-.102.683-.294.97l-1.703 2.556a.018.018 0 00-.003.01l.001.006c0 .002.002.004.004.006a.017.017 0 00.006.004l.007.001h10.964l.007-.001a.016.016 0 00.006-.004.016.016 0 00.004-.006l.001-.007a.017.017 0 00-.003-.01l-1.703-2.554a1.75 1.75 0 01-.294-.97V5A3.5 3.5 0 008 1.5zM3 5a5 5 0 0110 0v2.947c0 .05.015.098.042.139l1.703 2.555A1.518 1.518 0 0113.482 13H2.518a1.518 1.518 0 01-1.263-2.36l1.703-2.554A.25.25 0 003 7.947V5z"></path></svg>',
         sort: "common",
         list: [{
@@ -9813,7 +9813,7 @@ option {
         action: (url) => {
             var _a;
             if (!((_a = window.player) === null || _a === void 0 ? void 0 : _a.setDanmaku))
-                return API.toast.warning("内部组件丢失，已停止！");
+                API.toast.warning("内部组件丢失！");;
             API.danmaku.onlineDanmaku(url);
         }
     });
@@ -9856,7 +9856,7 @@ option {
                 action: function () {
                     var _a;
                     if (!((_a = window.player) === null || _a === void 0 ? void 0 : _a.setDanmaku))
-                        return API.toast.warning("内部组件丢失，已停止！");
+                        API.toast.warning("内部组件丢失");
                     new API.AllDanmaku();
                 },
             }]
